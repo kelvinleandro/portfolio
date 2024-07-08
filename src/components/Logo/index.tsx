2e4945f388d515822461dc6766e1d90402497515
kelvinleandro <kelvinleandro5@gmail.com>
2024-07-08 16:20:34 -0300

    feat: add gradient effect in header logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,13 @@ import TypeWriter from "../TypeWriter";
 const Logo = () => {
   return (
     <Link href="/">
-      <TypeWriter text="<  Kelvin />" fontSize="lg" fontWeight="bold" />
+      <TypeWriter
+        text="<  Kelvin/>"
+        fontSize="lg"
+        fontWeight="bold"
+        bgGradient="linear(to-r, main.blueMarguerite, main.blue600)"
+        bgClip="text"
+      />
     </Link>
   );
 };
