33a4cc07d09d53ad0fdd764e9fdde5383e29d325
kelvinleandro <kelvinleandro5@gmail.com>
2024-09-14 16:24:01 -0300

    feat: add experience description

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -5,21 +5,21 @@ export const EXPERIENCES: Experience[] = [
     role: "Scientific Initiation Scholar",
     where: "Federal University of Ceará",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum orci non justo pharetra, sed volutpat risus varius. Maecenas vulputate.",
+      "Undergraduate researcher on the project 'Detection of Anomalies in Biosignals of Patients with Chagas Disease to Aid Cardiological Medical Diagnosis.'",
     when: "2024 - Present",
   },
   {
     role: "Scientific Initiation Scholar",
     where: "Federal University of Ceará",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum orci non justo pharetra, sed volutpat risus varius. Maecenas vulputate.",
+      "Undergraduate researcher working on the project 'Detection of Epileptic Seizures Using Mahalanobis Distance and PCA', aiming to utilize machine learning (MLP) and statistical techniques to train a model capable of identifying epileptic seizures, which will be integrated into a remote patient monitoring system.",
     when: "2023-2024",
   },
   {
     role: "Project Advisor",
     where: "GTi Engenharia Jr.",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum orci non justo pharetra, sed volutpat risus varius. Maecenas vulputate.",
+      "Project advisor at the junior enterprise of the Computer Engineering course at UFC, where I develop websites using HTML, CSS, TypeScript, and React.",
     when: "2023",
   },
 ];
